a743a27afe8ed600d0caaa881026af0ec883f782
diamond2sword <95223065+diamond2sword@users.noreply.github.com>
2023-01-24 12:45:21 +0800

    Update Automation_Custom_PreScript.sh

--- Automation_Custom_PreScript.sh
+++ Automation_Custom_PreScript.sh
@@ -1,7 +1,6 @@
 main () {
 	fix_fs_partition
 	configure_usb_for_vnc
-	install_setup_log_viewer
 }
 
 NET_CONF_DIR="/etc/network/interfaces.d"
@@ -24,44 +23,6 @@ eval << "EOF" | sed -r 's/^(\t| )+//g'
 	sed -ir '{
 		s/(rootwait)/\1 modules-load=dwc2,g_ether /
 	}' /boot/cmdline.txt
-EOF
-}
-
-install_setup_log_viewer () {
-cat << "EOF" | sed -r 's/^( |\t)+//g' > /etc/profile.d/setup_log_viewer.sh
-	main () {
-		force_display_log
-	}
-
-	LOG_PATH="/var/tmp/dietpi/logs/dietpi-automation_custom_script.log"
-	VIM_OPTS='-R +'
-	STOP_PHRASE="STOP_WAIT_FOR_SETUP"
-
-	force_display_log () {
-		create_vim_killer
-		while true; do {
-			is_log_complete && {
-				break
-			}
-			vim $LOG_PATH $VIM_OPTS
-		} done
-	}
-
-	create_vim_killer () { (
-		while true; do {
-			! is_log_complete && {
-				continue
-			}
-			killall vim
-			break
-		} done
-	) & }	
-
-	is_log_complete () {
-		[[ "$(sed -n '/'"$STOP_PHRASE"'/=' $LOG_PATH)" ]]
-	}
-
-	main
 EOF
 }
 
